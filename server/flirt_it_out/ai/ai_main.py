c8bbccab83c1ebac76beca2352372de2335d6870
ethan <graguton@gmail.com>
2023-10-21 22:06:05 -0400

    Very cool stuff. Will keep you updated.

--- server/flirt_it_out/ai/ai_main.py
+++ server/flirt_it_out/ai/ai_main.py
@@ -1,2 +1,16 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
 def run():
-    print("test")
+    device = "cuda"  # the device to load the model onto
+
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+    prompt = "My favourite condiment is"
+
+    model_inputs = tokenizer([prompt], return_tensors="pt").to(device)
+    model.to(device)
+
+    generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample=True)
+    output = tokenizer.batch_decode(generated_ids)[0]
+    print(output)
