4d7f0a24973289ebb661d230732b1465d3d8936c
enamchae <56808335+enamchae@users.noreply.github.com>
2023-10-22 00:26:52 -0400

    feat: change register to form

--- web/src/lib/pages/NamePage.svelte
+++ web/src/lib/pages/NamePage.svelte
@@ -5,12 +5,11 @@ import heart from "$/images/heart.svg";
 
 let name = "";
 
-const onSubmitName = () => {
-    if (name.length === 0) return;
+// const onSubmitName = async () => {
+//     if (name.length === 0) return;
 
-    $username = name;
-    goto("/");
-};
+//     $username = name;
+// };
 </script>
 
 <div>
@@ -19,15 +18,17 @@ const onSubmitName = () => {
     <label class="strong-label"
             for="name-input">My name is</label>
     
-    <input type="text"
-            bind:value={name}
-            placeholder="your name"
-            id="name-input" />
+    <form action="/register" method="post">
+        <input type="text"
+                bind:value={name}
+                placeholder="your name"
+                name="name"
+                id="name-input" />
 
-    <div>
-        <button on:click={onSubmitName}
-                disabled={name.length === 0}>Next</button>
-    </div>
+        <input type="submit"
+                value="Register"
+                disabled={name.length === 0} />
+    </form>
 </div>
 
 <style lang="scss">
@@ -36,7 +37,13 @@ div {
     flex-flow: column;
     align-items: flex-start;
 
-    gap: 1rem;
+    > form {
+        display: flex;
+        flex-flow: column;
+
+        gap: 1rem;
+    }
+
 
     > * {
         padding: 0 1.5rem;
