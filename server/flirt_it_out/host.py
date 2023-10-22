b828bedbdc6f20305bca78933f646701388e9c9e
memsdm05 <bnbrowner@gmail.com>
2023-10-22 06:34:10 -0400

    changed stuff

--- server/flirt_it_out/host.py
+++ server/flirt_it_out/host.py
@@ -1,4 +1,4 @@
-from .common import PacketAgent, ZERO_UUID
+from .common import PacketAgent, ZERO_UUID, RoomState
 
 class Host(PacketAgent):
     def __init__(self) -> None:
@@ -9,7 +9,7 @@ class Host(PacketAgent):
         }
 
     async def handle_start_game(self, room, packet):
-        pass
+        room.state = RoomState.GAME
 
     async def handle_anim_end(self, room):
         pass
