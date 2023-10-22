265cc06b7b98173617e79bbe860e0998aadd7631
ethan <graguton@gmail.com>
2023-10-22 07:08:46 -0400

    Test Personalities

--- server/flirt_it_out/bot.py
+++ server/flirt_it_out/bot.py
@@ -2,4 +2,7 @@ class Bot:
     def __init__(self):
         self.personalities = {}
 
-    
+    def read_personalities(self, path):
+        with open(path) as f:
+            for line in f.readlines():
+                print(line)
